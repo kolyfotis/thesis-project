22698a28d00338504d62c63710ecf0a4fdc06247
Fotis.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2023-07-30 17:13:23 +0200

    Test data for Cars table

---
+++ 01-web-app/database/test-data/01_insert_into_cars.sql
@@ -0,0 +1,32 @@
+insert into cars (
+    make,model,body_type,fuel_type,price,horsepower,gearbox,door_number,color)
+values
+    ('Audi','TT','Coupe','Gasoline',50000,197,'Automatic',3,'White'),
+    ('Audi','A6','Sedan','Diesel',57000,204,'Automatic',5,'Grey'),
+    ('Audi','Q8','SUV','Electric',82000,340,'Automatic',5,'Grey'),
+    ('Audi','SQ7','SUV','Gasoline',99000,507,'Manual',5,'Black'),
+
+    ('BMW','118','Sedan','Gasoline',43000,136,'Automatic',5,'Black'),
+    ('BMW','840','Coupe','Gasoline',84000,333,'Automatic',5,'Grey'),
+    ('BMW','X6','SUV','Diesel',93000,286,'Automatic',5,'Black'),
+    ('BMW','iX','SUV','Electric',98000,523,'Automatic',5,'Black'),
+
+    ('Ford','Mustang','Coupe','Gasoline',60000,449,'Automatic',3,'Black'),
+    ('Ford','Mustang Mach-E','SUV','Electric',58000,294,'Automatic',5,'White'),
+    ('Ford','Fiesta','Sedan','Gasoline',26000,125,'Manual',5,'Black'),
+    ('Ford','Focus','Sedan','Gasoline',28000,125,'Manual',5,'Blue'),
+
+    ('Mercedes','G 63','SUV','Gasoline',199000,585,'Automatic',5,'Black'),
+    ('Mercedes','GLS 600','SUV','Gasoline',157000,557,'Automatic',5,'Black'),
+    ('Mercedes','A 250','Compact','Gasoline',54000,224,'Automatic',5,'Grey'),
+    ('Mercedes','B Electric Drive','Compact','Electric',14000,179,'Automatic',5,'Grey'),
+
+    ('Opel','Corsa','Compact','Diesel',20000,102,'Manual',5,'Blue'),
+    ('Opel','Corsa-e','Compact','Electric',37000,136,'Automatic',5,'White'),
+    ('Opel','Mokka','SUV','Electric',39000,136,'Automatic',5,'Grey'),
+    ('Opel','Grandland','SUV','Gasoline',38000,131,'Manual',5,'Black'),
+
+    ('Volkswagen','e-Golf','Sedan','Electric',19000,136,'Automatic',5,'Blue'),
+    ('Volkswagen','Golf','Sedan','Gasoline',29000,150,'Automatic',5,'White'),
+    ('Volkswagen','T-Cross','SUV','Gasoline',25000,150,'Automatic',5,'White'),
+    ('Volkswagen','ID.4','SUV','Gasoline',54000,204,'Automatic',5,'Blue');
