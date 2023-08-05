ac1e57e0eee48b22d6cec60e6cdfa41d9118038d
Fotis.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2023-08-05 21:02:09 +0200

    Refactored code to reduce IDE warnings

--- 01-web-app/src/main/java/com/fotis/thesis/service/CarService.java
+++ 01-web-app/src/main/java/com/fotis/thesis/service/CarService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CarService {
-public List<Car> findAll();
+List<Car> findAll();
 
-public Optional<Car> findById(Integer id);
+Optional<Car> findById(Integer id);
 }
