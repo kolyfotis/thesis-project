74c560dada4051cf0219ee609df1b169e149c6e3
Fotis.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2023-08-05 18:40:15 +0200

    Added Login & Logout functionality

---
+++ 01-web-app/src/main/java/com/fotis/thesis/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.fotis.thesis.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+@Bean
+public UserDetailsManager userDetailsManager(DataSource dataSource) {
+  return new JdbcUserDetailsManager(dataSource);
+}
+
+@Bean
+public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  http.authorizeHttpRequests(configurer ->
+    configurer
+      .requestMatchers("/admin/**").hasRole("ADMIN")
+      .requestMatchers("/webjars/**").permitAll()
+      .requestMatchers("/cars/**").permitAll()
+      .anyRequest().authenticated()
+  ).formLogin(form ->
+    form
+      .loginPage("/showLoginPage")
+      .loginProcessingUrl("/authenticateTheUser")
+      .permitAll()
+  ).logout(LogoutConfigurer::permitAll
+  );
+
+  return http.build();
+}
+}
