f4a5c894d63d94ac6fff6b0dc4529d650eb33fc5
Fotis.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2023-08-08 14:14:46 +0200

    Added console message to see when spring-boot-devtools restart the app

--- 01-web-app/src/main/java/com/fotis/thesis/ThesisApplication.java
+++ 01-web-app/src/main/java/com/fotis/thesis/ThesisApplication.java
@@ -3,10 +3,13 @@ package com.fotis.thesis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Calendar;
+
 @SpringBootApplication
 public class ThesisApplication {
 
 	public static void main(String[] args) {
+		System.out.printf("%s: Starting Application...%n", Calendar.getInstance().getTime());
 		SpringApplication.run(ThesisApplication.class, args);
 	}
 
