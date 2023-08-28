c16dbbeb84b983bf4531a2831382fe9af5ed14ba
Fotis.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2023-08-28 11:56:00 +0200

    Car detail page

--- 01-web-app/src/main/java/com/fotis/thesis/controller/CarController.java
+++ 01-web-app/src/main/java/com/fotis/thesis/controller/CarController.java
@@ -5,9 +5,11 @@ import com.fotis.thesis.service.CarService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/cars")
@@ -27,5 +29,28 @@ public String listCars(Model model) {
   model.addAttribute("cars", cars);
 
   return "car-list";
+}
+
+@GetMapping("/{id}")
+public String carDetails(Model model, @PathVariable String id) {
+
+  int parsedId;
+
+  try {
+    parsedId = Integer.parseInt(id);
+  } catch (NumberFormatException e) {
+    parsedId = 0;
+  }
+
+  Optional<Car> car = carService.findById(parsedId);
+
+  if (car.isEmpty())
+    return "error/404";
+
+  model.addAttribute("car", (
+      car.orElseGet(Car::new))
+  );
+
+  return "car-detail";
 }
 }
