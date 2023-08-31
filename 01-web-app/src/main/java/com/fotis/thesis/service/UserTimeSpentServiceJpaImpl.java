cc92e975a02972d92379f3d76d61bf5e9b1a9fc7
Fotis.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2023-08-31 19:21:27 +0200

    Added calls UserDataNormalizedService to evaluate normalized data & save it. Added username validation

--- 01-web-app/src/main/java/com/fotis/thesis/service/UserTimeSpentServiceJpaImpl.java
+++ 01-web-app/src/main/java/com/fotis/thesis/service/UserTimeSpentServiceJpaImpl.java
@@ -4,10 +4,13 @@ import com.fotis.thesis.entity.Car;
 import com.fotis.thesis.entity.UserData;
 import com.fotis.thesis.pojo.UserTimeSpentOnCar;
 import com.fotis.thesis.util.CarUtil;
+import com.fotis.thesis.util.UserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -15,36 +18,48 @@ import java.util.Optional;
 public class UserTimeSpentServiceJpaImpl implements UserTimeSpentService {
 private final CarService carService;
 private final UserDataService userDataService;
+private final UserDataNormalizedService userDataNormalizedService;
+private final UserService userService;
+
 public static final BigInteger MINIMUM_TIME_TO_TRACK_IN_MILLISECONDS = BigInteger.valueOf(5 * 1000);
 public static final BigInteger MAXIMUM_TIME_TO_TRACK_IN_MILLISECONDS = BigInteger.valueOf(15 * 60 * 1000);
 
 @Autowired
-public UserTimeSpentServiceJpaImpl(CarService carService, UserDataService userDataService) {
+public UserTimeSpentServiceJpaImpl(CarService carService, UserDataService userDataService,
+                                   UserDataNormalizedService userDataNormalizedService,
+                                   UserService userService) {
   this.carService = carService;
   this.userDataService = userDataService;
+  this.userDataNormalizedService = userDataNormalizedService;
+  this.userService = userService;
 }
 
 
 /**
  * <p>Saves the user's time spent for each tracked field-value pair using the UserDataService.</p>
- * <p>Ensures that the minimum time spent is more that the minimum time required to track user's time spent,
- * and that the maximum time spent does not exceed the maximum acceptable time to track.</p>
+ * <p>Validates the username. Ensures that the minimum time spent is more that the minimum time required
+ * to track user's time spent, and that the maximum time spent does not exceed the maximum acceptable time to track.</p>
  * <p>Retrieves the car, and a Map with the tracked field-value pairs.</p>
  * <p>Iterates over the Map and checks for each entry if the username, fieldName and fieldValue combination already
  * exists in the user data table. If it does, it will add the current time spent to the time spent field and update
  * the row. If it doesn't it will insert a new user data row.</p>
+ * <p>Finally calls the normalizeUserData method with the User data that changed to calculate the new values, and
+ * then the saveAll method to save the new values</p>
  *
  * @param   userTimeSpentOnCar  a POJO that contains username, carId and time spent viewing the car in milliseconds
  * @see     com.fotis.thesis.util.CarUtil#getTrackedColumnNamesAndValues(Car)
  * @see     com.fotis.thesis.service.UserDataServiceJpaImpl#findByUsernameAndFieldNameAndFieldValue(String, String, String)
+ * @see     com.fotis.thesis.service.UserDataNormalizedServiceJpaImpl#normalizeUserData(List)
+ *
  * */
 @Override
 public void saveUserData(UserTimeSpentOnCar userTimeSpentOnCar) {
   String username = userTimeSpentOnCar.getUsername();
   BigInteger timeSpentInMillis = userTimeSpentOnCar.getTimeSpentInMillis();
-  Optional<Car> car = carService.findById(userTimeSpentOnCar.getCarId());
+  Boolean userExists = UserUtil.userExists(username, userService);
 
-  if (timeSpentInMillis.compareTo(MINIMUM_TIME_TO_TRACK_IN_MILLISECONDS) < 0) {
+  if (timeSpentInMillis.compareTo(MINIMUM_TIME_TO_TRACK_IN_MILLISECONDS) < 0
+      || !userExists) {
     return;
   }
 
@@ -52,8 +67,11 @@ public void saveUserData(UserTimeSpentOnCar userTimeSpentOnCar) {
     timeSpentInMillis = MAXIMUM_TIME_TO_TRACK_IN_MILLISECONDS;
   }
 
+  Optional<Car> car = carService.findById(userTimeSpentOnCar.getCarId());
+
   if (car.isPresent()) {
     Map<String, String> trackedColumnNames = CarUtil.getTrackedColumnNamesAndValues(car.get());
+    List<UserData> userDataList = new ArrayList<>();
 
     for (Map.Entry<String, String> column : trackedColumnNames.entrySet()) {
       String fieldName = column.getKey();
@@ -65,14 +83,14 @@ public void saveUserData(UserTimeSpentOnCar userTimeSpentOnCar) {
 
       if (userDataRow.isPresent()) {
         userDataRow.get().setTimeSpent(userDataRow.get().getTimeSpent().add(timeSpentInMillis));
-        userDataService.save(userDataRow.get());
+        userDataList.add(userDataService.save(userDataRow.get()));
       } else {
         UserData userData = new UserData(0, username, fieldName, fieldValue, timeSpentInMillis);
-        userDataService.save(userData);
+        userDataList.add(userDataService.save(userData));
       }
     }
 
-    // TODO: call service to normalize the values and store to the database
+    userDataNormalizedService.saveAll(userDataNormalizedService.normalizeUserData(userDataList));
   }
 }
 }
