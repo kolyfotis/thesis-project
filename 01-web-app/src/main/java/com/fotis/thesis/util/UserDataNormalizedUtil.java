15b2345a52f3421cc1142e1ebb5403768a31a8d1
Fotis.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2023-08-31 19:18:16 +0200

    Added UserDataNormalized Utility method to evaluate normalized values for each field value given a List of userData (e.g. each make a user has viewed)

---
+++ 01-web-app/src/main/java/com/fotis/thesis/util/UserDataNormalizedUtil.java
@@ -0,0 +1,78 @@
+package com.fotis.thesis.util;
+
+import com.fotis.thesis.entity.UserData;
+import com.fotis.thesis.entity.UserDataNormalized;
+import com.fotis.thesis.service.UserDataNormalizedService;
+import com.fotis.thesis.service.UserDataService;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserDataNormalizedUtil {
+
+/**
+ * <p>Normalizes userData timeSpentInMillis for each fieldValue (column name).</p>
+ * <p>Iterates over userData and for each field name, gets a List of all field values the user has viewed. For example
+ * a list of makes that the user has viewed. Also get a List of the normalized values, to edit if and entry already
+ * exists or add a new entry to the list if it doesn't already exist.</p>
+ * <p>Calculates the summary of the milliseconds the user spent on a field name (eg. make)</p>
+ * <p>Iterates over the values of this field that the user has viewed (eg. Audi, BMW)</p>
+ * <p>Checks if a normalized row already exists using an Optional<UserDataNormalized>.<br> If it does, then it sets its
+ * normalized time spent to the time spent in milliseconds viewing the field value (eg. Audi) divided by the total
+ * time spent viewing different makes.<br> If it doesn't, creates a new instance of a UserDataNormalized Object and
+ * similarly sets it normalized time spent.</p>
+ * <p>Finally adds the userDataNormalized row to the new List normalizedDataList, and returns it.</p>
+ *
+ * @param   userData                    a list of newly evaluated userData, to be used for evaluating the normalized
+ *                                      values for the tracked fields
+ * @param   userDataService             an instance of userDataService, used to query the userData entity and find all
+ *                                      values for a field name that a user has viewed (e.g. for a make)
+ * @param   userDataNormalizedService   an instance of userDataNormalizedService, used to query the userDataNormalized
+ *                                      entity and find all normalized values for a field name that a user has viewed,
+ *                                      if already exist (e.g. for a make)
+ *
+ * @see     com.fotis.thesis.service.UserDataServiceJpaImpl#findByUsernameAndFieldNameAndFieldValue(String, String, String)
+ * @see     com.fotis.thesis.service.UserDataNormalizedServiceJpaImpl#normalizeUserData(List)
+ *
+ * */
+public static List<UserDataNormalized> normalizeUserData(List<UserData> userData, UserDataService userDataService,
+                                                         UserDataNormalizedService userDataNormalizedService) {
+  List<UserDataNormalized> normalizedDataList = new ArrayList<>();
+
+  for (UserData fieldName : userData) {
+    List<UserData> fieldValues = userDataService.findByUsernameAndFieldName(
+        fieldName.getUsername(), fieldName.getFieldName());
+    List<UserDataNormalized> fieldValuesNormalized = userDataNormalizedService.findByUsernameAndFieldName(
+        fieldName.getUsername(), fieldName.getFieldName());
+    BigInteger sum = fieldValues.stream()
+        .map(UserData::getTimeSpent)
+        .reduce(BigInteger.ZERO, BigInteger::add);
+
+    for (UserData fieldValue : fieldValues) {
+      Optional<UserDataNormalized> userDataNormalizedRow = fieldValuesNormalized.stream().filter(
+          normalValue -> normalValue.getFieldValue().equals(fieldValue.getFieldValue())
+      ).findFirst();
+
+      UserDataNormalized userDataNormalized;
+
+      if (userDataNormalizedRow.isPresent()) {
+        userDataNormalizedRow.get().setTimeSpent(new BigDecimal(
+            fieldValue.getTimeSpent()).divide(new BigDecimal(sum), 9, RoundingMode.HALF_DOWN));
+
+        userDataNormalized = userDataNormalizedRow.get();
+      } else {
+        userDataNormalized = new UserDataNormalized(
+            0, fieldValue.getUsername(), fieldValue.getFieldName(), fieldValue.getFieldValue(), new BigDecimal(
+            fieldValue.getTimeSpent()).divide(new BigDecimal(sum), 9, RoundingMode.HALF_DOWN));
+      }
+
+      normalizedDataList.add(userDataNormalized);
+    }
+  }
+  return normalizedDataList;
+}
+}
