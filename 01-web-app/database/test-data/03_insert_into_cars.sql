fc56c14fcb7d0be553c11df45ebdb2706651d144
Fotis.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2023-11-29 20:47:20 +0100

    Added more cars to the database

---
+++ 01-web-app/database/test-data/03_insert_into_cars.sql
@@ -0,0 +1,15 @@
+insert into cars (
+    make,model,body_type,fuel_type,price,horsepower,gearbox,door_number,color,engine_size,image)
+values
+    ('Audi','A4','Sedan','Gasoline',38000,150,'Manual',5,'Black', 1984,
+        'images/car-pictures/audi-a4-black.jpeg'),
+    ('BMW','420','Coupe','Diesel',71000,190,'Manual',2,'Grey', 1995,
+        'images/car-pictures/bmw-420-grey.jpeg'),
+    ('Ford','Focus','Sedan','Gasoline',47000,280,'Manual',5,'White', 2261,
+        'images/car-pictures/ford-focus-white.jpeg'),
+    ('Mercedes','CLA 200','Coupe','Gasoline',47300,163,'Automatic',5,'White', 1505,
+        'images/car-pictures/mercedes-cla-200-white.jpeg'),
+    ('Opel','Crossland','SUV','Gasoline',18800,110,'Manual',5,'White', 1199,
+        'images/car-pictures/opel-crossland-white.jpeg'),
+    ('Volkswagen','Taigo','Coupe','Gasoline',36000,150,'Manual',5,'Blue', 1498,
+        'images/car-pictures/volkswagen-taigo-blue.jpeg');
