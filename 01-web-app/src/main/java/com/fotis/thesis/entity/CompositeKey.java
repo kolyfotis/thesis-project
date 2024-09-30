17bc234bd84ffbcfd4b54e5cbf7cdb13ec07252d
Fotis.Kolytoumpas <fotis.kolytoumpas@gmail.com>
2024-09-30 18:51:15 +0200

    Added CompositeKey class to be used as ID in user_recommendations table, using 'username' and 'carId' fields

---
+++ 01-web-app/src/main/java/com/fotis/thesis/entity/CompositeKey.java
@@ -0,0 +1,10 @@
+package com.fotis.thesis.entity;
+
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+public class CompositeKey implements Serializable {
+private String username;
+private int carId;
+}
